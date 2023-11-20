5cc2edca4b01607421e3300ba89768a3b380f6ce
Lamont Crook <lamont@adobe.com>
2023-11-20 14:17:32 -0700

    update no margins

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,26 +1,3 @@
-function View() { }
-
-View.prototype = {
-  constructor: View,
-  isElementInView: function (element, fullyInView) {
-    const pageTop = document.documentElement.scrollTop;
-    const pageBottom = pageTop + window.screen.height;
-    const elementTop = element.getBoundingClientRect().top;
-    const elementBottom = elementTop + element.getBoundingClientRect().height;
-
-    console.log(`page top ${pageTop}`);
-    console.log(`page bottom ${pageBottom}`);
-    console.log(`element top ${elementTop}`);
-    console.log(`element bottom ${elementBottom}`);
-
-    if (fullyInView === true) {
-      return ((pageTop < elementTop) && (pageBottom > elementBottom));
-    } else {
-      return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
-    }
-  }
-}
-
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -34,10 +11,6 @@ export default function decorate(block) {
         if (picWrapper && picWrapper.children.length === 1) {
           // picture is only content in column
           picWrapper.classList.add('columns-img-col');
-          window.addEventListener('scroll', () => {
-            const isElementInView = new View().isElementInView(picWrapper, true);
-            console.log(isElementInView);
-          });
         }
       } else {
         const content = document.createElement('div');
