fd61ba7c8ffcbce156e7e802577c882523106f6e
Lamont Crook <lamont@adobe.com>
2023-12-20 10:30:42 -0700

    add pattern to card

--- templates/editor/editor.js
+++ templates/editor/editor.js
@@ -1,24 +1,68 @@
-export default function decorate(main) {
-  const cssVars = [...document.styleSheets].map((item) => {
-    if (item.href.includes('styles.css')) {
-      const rules = [...item.cssRules].reduce((accumulator, rule) => {
-        if (rule.selectorText === ':root') {
-          return (Object.values(rule.style));
-        }
-        return accumulator;
-      }, []);
-      console.log(rules);
-      return rules;
-    }
-  });
+const VARIABLES = 'https://main--demo-boilerplate--lamontacrook.hlx.page/theme/variables.json?sheet-variables';
 
-  console.log(cssVars);
+async function fetchVariables() {
+  const url = new URL(VARIABLES);
+  const { pathname } = url;
+  const variables = await fetch(pathname);
+  const json = await variables.json();
+  const variableMap = {};
+  
+  Object.values(json.data).forEach((row) => {
+    if(row.Block === '') row.Block = 'default';
+    else row.Block = row.Block.toLowerCase();
+    if(variableMap.hasOwnProperty(row.Block)) variableMap[row.Block].push(row);
+    else variableMap[row.Block] = [row];
+  });
+  return variableMap;
+}
+export default async function decorate(main) {
+  const variables = await fetchVariables();
   const blocks = main.querySelectorAll('.block');
   const form = document.createElement('div');
+  const tabs = document.createElement('ul');
+  form.append(tabs);
   form.classList.add('form');
   blocks.forEach((block) => {
     const blockName = [...block.classList].shift();
-    form.innerHTML += `<strong>${blockName}</strong>`;
+
+    const li = document.createElement('li');
+    const anchor = document.createElement('a');
+    anchor.setAttribute('href', `#${blockName}`);
+    anchor.textContent = blockName;
+    anchor.addEventListener('click', ((e) => {
+      form.querySelectorAll('.container').forEach((item) => item.setAttribute('style', 'visibility:hidden'));
+      const container = form.querySelector(`#container-${blockName}`);
+      console.log(container);
+      // container.setAttribute('style', 'visibility:visible');
+      container.setAttribute('style', 'display:block');
+      e.preventDefault();
+    }));
+    li.append(anchor);
+    tabs.append(li);
+
+    const container = document.createElement('div');
+    container.classList.add('container');
+    container.setAttribute('id', `container-${blockName}`);
+    variables[blockName] && variables[blockName].forEach((item) => {
+      const input = document.createElement('input');
+      const label = document.createElement('label');
+      label.classList.add('label-item');
+      label.setAttribute('for', item.Variable);
+      label.textContent = item.Name;
+      input.setAttribute('type', 'text');
+      input.setAttribute('id', item.Variable);
+      input.setAttribute('value', item.Value);
+      input.classList.add('input-item');
+      input.addEventListener('change', ((e) => {
+        const r = document.querySelector(':root');
+        r.style.setProperty(e.target.id, e.target.value);
+      }));
+      const inputGroup = document.createElement('div');
+      inputGroup.classList.add('input-group');
+      inputGroup.append(label, input);
+      container.append(inputGroup);
+    });
+    form.append(container);
   });
   main.append(form);
 }
