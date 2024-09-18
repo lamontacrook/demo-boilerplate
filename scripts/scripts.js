10737930ed4a60df87fa5fbd9a79c97b685a7784
Lamont Crook <lamont@adobe.com>
2024-09-18 10:11:41 -0600

    update DM

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,10 @@
 import {
   sampleRUM,
   buildBlock,
+  createOptimizedPicture as libCreateOptimizedPicture,
   loadHeader,
   loadFooter,
-  decorateButtons,
+  decorateButtons as libDecorateButtons,
   decorateIcons,
   decorateSections,
   decorateBlocks,
@@ -147,6 +148,91 @@ function buildAutoBlocks(main) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
   }
+}
+
+/*
+  * Appends query params to a URL
+  * @param {string} url The URL to append query params to
+  * @param {object} params The query params to append
+  * @returns {string} The URL with query params appended
+  * @private
+  * @example
+  * appendQueryParams('https://example.com', { foo: 'bar' });
+  * // returns 'https://example.com?foo=bar'
+*/
+function appendQueryParams(url, params) {
+  const { searchParams } = url;
+  params.forEach((value, key) => {
+    searchParams.set(key, value);
+  });
+  url.search = searchParams.toString();
+  return url.toString();
+}
+
+/**
+ * Creates an optimized picture element for an image.
+ * If the image is not an absolute URL, it will be passed to libCreateOptimizedPicture.
+ * @param {string} src The image source URL
+ * @param {string} alt The image alt text
+ * @param {boolean} eager Whether to load the image eagerly
+ * @param {object[]} breakpoints The breakpoints to use
+ * @returns {Element} The picture element
+ *
+ */
+export function createOptimizedPicture(src, alt = '', eager = false, breakpoints = [{ media: '(min-width: 600px)', width: '2000' }, { width: '750' }]) {
+  const isAbsoluteUrl = /^https?:\/\//i.test(src);
+
+  // Fallback to createOptimizedPicture if src is not an absolute URL
+  if (!isAbsoluteUrl) return libCreateOptimizedPicture(src, alt, eager, breakpoints);
+
+  const url = new URL(src);
+  const picture = document.createElement('picture');
+  const { pathname } = url;
+  const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
+
+  // webp
+  breakpoints.forEach((br) => {
+    const source = document.createElement('source');
+    if (br.media) source.setAttribute('media', br.media);
+    source.setAttribute('type', 'image/webp');
+    const searchParams = new URLSearchParams({ width: br.width, format: 'webply' });
+    source.setAttribute('srcset', appendQueryParams(url, searchParams));
+    picture.appendChild(source);
+  });
+
+  // fallback
+  breakpoints.forEach((br, i) => {
+    const searchParams = new URLSearchParams({ width: br.width, format: ext });
+
+    if (i < breakpoints.length - 1) {
+      const source = document.createElement('source');
+      if (br.media) source.setAttribute('media', br.media);
+      source.setAttribute('srcset', appendQueryParams(url, searchParams));
+      picture.appendChild(source);
+    } else {
+      const img = document.createElement('img');
+      img.setAttribute('loading', eager ? 'eager' : 'lazy');
+      img.setAttribute('alt', alt);
+      picture.appendChild(img);
+      img.setAttribute('src', appendQueryParams(url, searchParams));
+    }
+  });
+
+  return picture;
+}
+
+/**
+ * Builds all synthetic blocks in a container element.
+ * @param {Element} main The container element
+ */
+function decorateButtons(main) {
+  main.querySelectorAll('a').forEach((a) => {
+    if(a.href.startsWith('https://delivery-')) {
+      const picture = createOptimizedPicture(a.href);
+      a.replaceWith(picture);
+    } else libDecorateButtons(main);
+  });
+  
 }
 
 /**
